cf3ba9d840ebb0e57944f9f2344d86170f3d9128
diogenmishel@gmail.com <diogenmishel@gmail.com>
2024-04-14 11:31:03 +0300

    Добавьте файлы проекта.

---
+++ Mirages/Mirage.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO.Compression;
+using OTAPI;
+using Terraria;
+using Terraria.Net.Sockets;
+using TShockAPI;
+using Microsoft.Xna.Framework;
+
+namespace Mirages
+{
+    public class Mirage : IEnumerable<MirageTile>
+    {
+        MirageTile[,] Tiles;
+        Rectangle Area;
+
+        public int X => Area.X;
+        public int Y => Area.Y;
+        public short Width => (short)Area.Width;
+        public short Height => (short)Area.Height;
+
+        public MirageTile this[int x, int y]
+        {
+            get => Tiles[X - x, Y - y];
+        }
+
+        public Mirage(Rectangle area) 
+        {
+            Area = area;
+            Tiles = new MirageTile[Width, Height];
+
+            for (int i = 0; i < Width; i++)
+                for (int j = 0; j < Height; j++)
+                {
+                    Tiles[i, j] = new MirageTile(i + X, j + Y);
+                }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        public IEnumerator<MirageTile> GetEnumerator()
+        {
+            var list = new List<MirageTile>();
+            for (int j = 0; j < Height; j++)
+                for (int i = 0; i < Width; i++)
+                {
+                    list.Add(Tiles[i, j]);
+                }
+            return list.GetEnumerator();
+        }
+
+        public byte[] GetPacket10Data()
+        {
+            byte[] data;
+            using (var s = new MemoryStream())
+            {
+                using (var w = new BinaryWriter(s))
+                {
+                    w.Write(this);
+                }
+                data = s.ToArray();
+            }
+            using (var s = new MemoryStream())
+            {
+                using (var w = new DeflateStream(s, CompressionMode.Compress, true))
+                {
+                    w.Write(data, 0, data.Length);
+                }
+                data = s.ToArray();
+            }
+            using (var s = new MemoryStream())
+            {
+                using (var w = new BinaryWriter(s))
+                {
+                    w.BaseStream.Position = 2;
+                    w.Write((byte)10);
+                    w.Write(data);
+                    var length = (ushort)w.BaseStream.Position;
+                    w.BaseStream.Position = 0;
+                    w.Write(length);
+                }
+                return s.ToArray();
+            }
+        }
+
+        public void SendAll(bool tileframe, Func<TSPlayer, bool> predicate = null)
+        {
+            Send(tileframe, predicate == null ? 
+                TShock.Players : 
+                TShock.Players.Where(i => predicate(i)).ToArray());
+        }
+
+        public void Send(bool tileframe, params TSPlayer[] players)
+        {
+            if (players == null || players.Length == 0 || players.All(i => i == null || !i.ConnectionAlive))
+            {
+                return;
+            }
+            var data = GetPacket10Data();
+
+            foreach (var i in players)
+            {
+                if (i == null || !i.ConnectionAlive)
+                    continue;
+
+                var socket = Netplay.Clients[i.Index].Socket;
+                var callback = (SocketSendCallback)Netplay.Clients[i.Index].ServerWriteCallBack;
+
+                Hooks.NetMessage.InvokeSendBytes(
+                    socket,
+                    data,
+                    0,
+                    data.Length,
+                    callback,
+                    null,
+                    i.Index);
+
+                if (!tileframe)
+                    continue;
+
+                i.SendData(
+                    PacketTypes.TileFrameSection, 
+                    null, 
+                    Netplay.GetSectionX(X), 
+                    Netplay.GetSectionY(Y), 
+                    Netplay.GetSectionX(X + Width), 
+                    Netplay.GetSectionY(Y + Height));
+            }
+        }
+    }
+
+    public class MirageTile : Tile
+    {
+        public int X;
+        public int Y;
+
+        public MirageTile(int x, int y) : base(Main.tile[x, y] ?? new Tile())
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
---
+++ Mirages/Utils.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+
+namespace Mirages
+{
+    public static class Utils
+    {
+        public static void Write(this BinaryWriter w, Mirage mirage)
+        {
+            var last = default(ITile);
+            var same = default(short);
+            var data = new List<byte>();
+            var flag = Flag1.None;
+
+            w.Write(mirage.X);
+            w.Write(mirage.Y);
+            w.Write(mirage.Width);
+            w.Write(mirage.Height);
+
+            foreach (var i in mirage)
+            {
+                if (i.isTheSameAs(last) && TileID.Sets.AllowsSaveCompressionBatching[i.type])
+                {
+                    same++;
+                    continue;
+                }
+                if (last != null)
+                {
+                    w.Write(data, ref same, ref flag);
+                }
+                last = WriteTile(i, ref flag, data);
+            }
+            w.Write(data, ref same, ref flag);
+            w.Write((short)0); // chests count
+            w.Write((short)0); // signs count
+            w.Write((short)0); // entities count 
+        }
+
+        static void Write(this BinaryWriter w, List<byte> data, ref short same, ref Flag1 flag1)
+        {
+            if (same > 0)
+            {
+                var bytes = BitConverter.GetBytes(same);
+                if (bytes[1] > 0)
+                {
+                    data.AddRange(bytes);
+                    flag1 |= Flag1.TwoBytesSame;
+                }
+                else
+                {
+                    data.Add(bytes[0]);
+                    flag1 |= Flag1.OneByteSame;
+                }
+            }
+            data.Insert(0, (byte)flag1);
+            w.Write(data.ToArray(), 0, data.Count);
+            flag1 = Flag1.None;
+            same = 0;
+        }
+
+        static ITile WriteTile(ITile tile, ref Flag1 flag1, List<byte> data)
+        {
+            var flag2 = Flag2.None;
+            var flag3 = Flag3.None;
+            var flag4 = Flag4.None;
+
+            data.Clear();
+            WriteBlock(data, tile, ref flag1, ref flag3);
+            WriteWall(data, tile, ref flag1, ref flag3);
+            WriteLiquid(data, tile, ref flag1, ref flag3);
+            WriteWires(tile, ref flag2, ref flag3);
+            WriteSlopes(tile, ref flag2);
+            WriteOther(data, tile, ref flag3, ref flag4);
+            WriteFlags(data, ref flag1, ref flag2, ref flag3, ref flag4);
+            return tile;
+        }
+
+        static void WriteBlock(List<byte> data, ITile tile, ref Flag1 flag1, ref Flag3 flag3)
+        {
+            if (!tile.active())
+            {
+                return;
+            }
+            flag1 |= Flag1.Block;
+            var bytes = BitConverter.GetBytes(tile.type);
+            data.Add(bytes[0]);
+
+            if (bytes[1] > 0)
+            {
+                data.Add(bytes[1]);
+                flag1 |= Flag1.TwoBytesBlock;
+            }
+            if (Main.tileFrameImportant[tile.type])
+            {
+                data.AddRange(BitConverter.GetBytes(tile.frameX));
+                data.AddRange(BitConverter.GetBytes(tile.frameY));
+            }
+            if (tile.color() != 0)
+            {
+                flag3 |= Flag3.BlockColor;
+                data.Add(tile.color());
+            }
+        }
+
+        static void WriteWall(List<byte> data, ITile tile, ref Flag1 flag1, ref Flag3 flag3)
+        {
+            if (tile.wall == 0)
+            {
+                return;
+            }
+            flag1 |= Flag1.Wall;
+            data.Add((byte)tile.wall);
+
+            if (tile.wallColor() > 0)
+            {
+                flag3 |= Flag3.WallColor;
+                data.Add(tile.wallColor());
+            }
+        }
+
+        static void WriteLiquid(List<byte> data, ITile tile, ref Flag1 flag1, ref Flag3 flag3)
+        {
+            if (tile.liquid > 0)
+            {
+                return;
+            }
+            if (tile.shimmer())
+            {
+                flag3 |= Flag3.Shimmer;
+                flag1 |= Flag1.Water;
+            }
+            else if (tile.lava())
+            {
+                flag1 |= Flag1.Lava;
+            }
+            else if (tile.honey())
+            {
+                flag1 |= Flag1.Water | Flag1.Lava;
+            }
+            else
+            {
+                flag1 |= Flag1.Water;
+            }
+            data.Add(tile.liquid);
+        }
+
+        static void WriteWires(ITile tile, ref Flag2 flag2, ref Flag3 flag3)
+        {
+            if (tile.wire())
+            {
+                flag2 |= Flag2.WireR;
+            }
+            if (tile.wire2())
+            {
+                flag2 |= Flag2.WireG;
+            }
+            if (tile.wire3())
+            {
+                flag2 |= Flag2.WireB;
+            }
+            if (tile.wire4())
+            {
+                flag3 |= Flag3.WireY;
+            }
+            if (tile.actuator())
+            {
+                flag3 |= Flag3.Actuator;
+            }
+        }
+
+        static void WriteSlopes(ITile tile, ref Flag2 flag2)
+        {
+            if (tile.halfBrick())
+            {
+                flag2 |= Flag2.HalfBrick;
+            }
+            switch (tile.slope())
+            {
+                case 1:
+                    flag2 |= Flag2.Slope1;
+                    break;
+                case 2:
+                    flag2 |= Flag2.Slope1 | Flag2.HalfBrick;
+                    break;
+                case 3:
+                    flag2 |= Flag2.Slope3;
+                    break;
+                case 4:
+                    flag2 |= Flag2.Slope3 | Flag2.HalfBrick;
+                    break;
+            }
+        }
+
+        static void WriteOther(List<byte> data, ITile tile, ref Flag3 flag3, ref Flag4 flag4)
+        {
+            if (tile.wall > 255)
+            {
+                data.Add(BitConverter.GetBytes(tile.wall)[1]);
+                flag3 |= Flag3.TwoBytesWall;
+            }
+            if (tile.invisibleBlock())
+            {
+                flag4 |= Flag4.InvisibleBlock;
+            }
+            if (tile.invisibleWall())
+            {
+                flag4 |= Flag4.InvisibleWall;
+            }
+            if (tile.fullbrightBlock())
+            {
+                flag4 |= Flag4.FullBrightBlock;
+            }
+            if (tile.fullbrightWall())
+            {
+                flag4 |= Flag4.FullBrightWall;
+            }
+            if (tile.inActive())
+            {
+                flag3 |= Flag3.InActive;
+            }
+        }
+
+        static void WriteFlags(List<byte> data, ref Flag1 flag1, ref Flag2 flag2, ref Flag3 flag3, ref Flag4 flag4)
+        {
+            if (flag4 != Flag4.None)
+            {
+                flag3 |= Flag3.Flag4;
+                data.Insert(0, (byte)flag4);
+            }
+            if (flag3 != Flag3.None)
+            {
+                flag2 |= Flag2.Flag3;
+                data.Insert(0, (byte)flag3);
+            }
+            if (flag2 != Flag2.None)
+            {
+                flag1 |= Flag1.Flag2;
+                data.Insert(0, (byte)flag2);
+            }
+        }
+
+        enum Flag1 : byte
+        {
+            None = 0,
+            Flag2 = 1,
+            Block = 2,
+            Wall = 4,
+            Water = 8,
+            Lava = 16,
+            TwoBytesBlock = 32,
+            OneByteSame = 64,
+            TwoBytesSame = 128,
+        }
+        enum Flag2 : byte
+        {
+            None = 0,
+            Flag3 = 1,
+            WireR = 2,
+            WireG = 4,
+            WireB = 8,
+            HalfBrick = 16,
+            Slope1 = 32,
+            Slope3 = 64,
+        }
+        enum Flag3 : byte
+        {
+            None = 0,
+            Flag4 = 1,
+            Actuator = 2,
+            InActive = 4,
+            BlockColor = 8,
+            WallColor = 16,
+            WireY = 32,
+            TwoBytesWall = 64,
+            Shimmer = 128,
+        }
+        enum Flag4 : byte
+        {
+            None = 0,
+            InvisibleBlock = 2,
+            InvisibleWall = 4,
+            FullBrightBlock = 8,
+            FullBrightWall = 16,
+        }
+    }
+}
